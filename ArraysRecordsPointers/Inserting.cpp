69254386048342cdc6c3713b0d812edbba300a3d
mehedihasanshakil7 <118498349+mehedihasanshakil7@users.noreply.github.com>
2022-11-18 16:10:45 +0600

    Various insertion operation on linear array

--- ArraysRecordsPointers/Inserting.cpp
+++ ArraysRecordsPointers/Inserting.cpp
@@ -5,11 +5,11 @@ void insert_name_in_sorted_list();
 void insert_number_in_kth_position();
 void insert_name_in_kth_position();
 
-int main(void) {
-    insert_name_in_sorted_list();
-    insert_number_in_sorted_list();
-    //insert_number_in_kth_position();
+int main(void) { //Call only one function at a time
     insert_name_in_kth_position();
+    //insert_name_in_sorted_list();
+    //insert_number_in_sorted_list();
+    //insert_number_in_kth_position();
     return 0;
 }
 
@@ -18,6 +18,11 @@ void insert_number_in_sorted_list() {
     int n = 10, i, item, array[15] = {32, 38, 41, 46, 49, 53, 57, 59, 62, 64};
     printf("\nEnter the number to be inserted into a sorted list: ");
     cin>>item;
+    cout<<"\nArray before inserting the item:\n";
+    for(i = 0; i < n; i++) {
+        cout<<array[i]<<" ";
+    }
+    cout<<"\n\n";
     for(i = 0; i < n; i++) {
         if(item < array[i]) break;
     }
@@ -26,7 +31,7 @@ void insert_number_in_sorted_list() {
     }
     array[i] = item;
     n = n + 1;
-    cout<<"Array after inserting "<<item<<" is: ";
+    cout<<"Array after inserting "<<item<<" is:\n";
     for(i = 0; i < n; i++) {
         cout<<array[i]<<" ";
     }
@@ -39,7 +44,12 @@ void insert_name_in_sorted_list() {
     string name;
     string names[15] = {"Aishwarya", "Katrina", "Keerthy", "Parineeti", "Pooja", "Rashmika", "Zareen"};
     cout<<"\nEnter the name to be inserted: ";
-    cin>>name;
+    getline(cin, name);
+    cout<<"\nArray before inserting the name:\n";
+    for(int i = 0; i < m; i++) {
+        cout<<names[i]<<" ";
+    }
+    cout<<"\n\n";
     for(i = 0; i < m; i++) {
         if(name.compare(names[i]) < 0) break;
     }
@@ -48,7 +58,7 @@ void insert_name_in_sorted_list() {
     }
     names[i] = name;
     m = m + 1;
-    cout<<"Array after inserting "<<name<<" is: ";
+    cout<<"Array after inserting "<<name<<" is:\n";
     for(int j = 0; j < m; j++) {
         cout<<names[j]<<" ";
     }cout<<"\n\n";
@@ -58,14 +68,21 @@ void insert_name_in_sorted_list() {
 void insert_name_in_kth_position() {
     int n = 7, position;
     string name, names[15] = {"Aishwarya", "Katrina", "Keerthy", "Parineeti", "Pooja", "Rashmika", "Zareen"};
-    cout<<"\nEnter a name and its position: ";
-    cin>>name>>position;
+    cout<<"\nEnter a name: ";
+    getline(cin, name);
+    cout<<"\nEnter its position: ";
+    cin>>position;
+    cout<<"\nArray before inserting the name:\n";
+    for(int i = 0; i < n; i++) {
+        cout<<names[i]<<" ";
+    }
+    cout<<"\n\n";
     for(int i = n; i >= position; i--) {
         names[i] = names[i-1];
     }
     names[position-1] = name;
     n = n + 1;
-    cout<<"Array after inserting "<<name<<" at position "<<position<<" is: ";
+    cout<<"Array after inserting "<<name<<" at position "<<position<<" is:\n";
     for(int j = 0; j < n; j++) {
         cout<<names[j]<<" ";
     }cout<<"\n\n";
@@ -76,6 +93,11 @@ void insert_number_in_kth_position() {
     int n = 5, item, position, array[10] = {1, 2, 3, 4, 5};
     cout<<"\nEnter a number and its position: ";
     cin>>item>>position;
+    cout<<"\nArray before inserting the item:\n";
+    for(int i = 0; i < n; i++) {
+        cout<<array[i]<<" ";
+    }
+    cout<<"\n\n";
     for(int i = n; i >= position; i--) {
         array[i] = array[i-1];
     }
