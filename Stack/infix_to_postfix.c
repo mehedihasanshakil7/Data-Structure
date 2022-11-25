d34f6965646bfad43b193d3fd85e1fed3f08fbe6
Mehedi Hasan Shakil <118498349+mehedihasanshakil7@users.noreply.github.com>
2022-11-26 00:03:35 +0600

    Infix to postfix notation

--- Stack/infix_to_postfix.c
+++ Stack/infix_to_postfix.c
@@ -8,32 +8,46 @@ void push(char);
 char pop();
 char peek();
 int precedence(char);
+int isEmpty();
 
-int main() {
+int main()
+{
     char infix[100], postfix[100];
     puts("\nEnter an expression:");
     gets(infix);
-    strcat(infix, ")"); //Add ")" as sentinel
+    strcat(infix, ")"); // Add ")" as sentinel
     push('(');
     int i = 0, k = 0;
-    while(top != -1) { //Is stack empty?
-        if((infix[i] >= 'A' && infix[i] <= 'Z') || (infix[i] >= 'a' && infix[i] <= 'z')) {
+    while (!isEmpty()) //Is stack empty?
+    {
+        if (infix[i] == ' ')
+        {
+            i++;
+            continue;
+        }
+        else if ((infix[i] >= 'A' && infix[i] <= 'Z') || (infix[i] >= 'a' && infix[i] <= 'z'))
+        {
             postfix[k++] = infix[i];
         }
-        else if(infix[i] == '(') {
+        else if (infix[i] == '(')
+        {
             push('(');
         }
-        else if(infix[i] == ')') {
-            while(peek() != '(') {
-                postfix[k++] = pop(); //Pop all operators
+        else if (infix[i] == ')')
+        {
+            while (peek() != '(')
+            {
+                postfix[k++] = pop(); // Pop all operators
             }
-            pop(); //Pop "("
+            pop(); // Pop "("
         }
-        else {
-            while(precedence(peek()) >= precedence(infix[i])) {
-                postfix[k++] = pop(); //Operators of same or higher precedence are popped out
+        else
+        {
+            while (precedence(peek()) >= precedence(infix[i]))
+            {
+                postfix[k++] = pop(); // Operators of same or higher precedence are popped out
             }
-            push(infix[i]); //Push the new operator to stack
+            push(infix[i]); // Push the new operator to stack
         }
         i++;
     }
@@ -44,42 +58,66 @@ int main() {
     return 0;
 }
 
-void push(char ch) {
-    if(top == MAX-1) {
+void push(char ch)
+{
+    if (top == MAX - 1)
+    {
         puts("\nStack Overflow\n");
     }
-    else {
+    else
+    {
         top++;
         stack[top] = ch;
     }
 }
-char pop() {
-    if(top == -1) {
+char pop()
+{
+    if (top == -1)
+    {
         puts("\nStack Underflow\n");
         return '\0';
     }
-    else {
+    else
+    {
         char ch1 = stack[top];
         top--;
         return ch1;
     }
 }
-char peek() {
-    if(top == -1) {
+char peek()
+{
+    if (top == -1)
+    {
         puts("\nThe Stack is empty\n");
         return '\0';
     }
-    else {
+    else
+    {
         return stack[top];
     }
 }
-int precedence(char ch) {
-    switch(ch) {
-        case '^': return 3;
-        case '*': return 2;
-        case '/': return 2;
-        case '+': return 1;
-        case '-': return 1;
-        default: return 0;
+int isEmpty()
+{
+    if (top == -1)
+        return 1;
+    else
+        return 0;
+}
+int precedence(char ch)
+{
+    switch (ch)
+    {
+    case '^':
+        return 3;
+    case '*':
+        return 2;
+    case '/':
+        return 2;
+    case '+':
+        return 1;
+    case '-':
+        return 1;
+    default:
+        return 0;
     }
 }
