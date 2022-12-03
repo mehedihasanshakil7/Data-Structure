afd518f8a5e44bf11b35bb68c2ecb2b8b0adbb6c
Mehedi Hasan Shakil <118498349+mehedihasanshakil7@users.noreply.github.com>
2022-12-03 17:10:42 +0600

    Evaluation of postfix notation (including variables and floating-point numbers)

--- Stack/postfix_evaluation.c
+++ Stack/postfix_evaluation.c
@@ -1,3 +1,11 @@
+/* Input Output format
+	Enter the postfix expression: A B - 3.9 + 3 / C ^
+	Enter the value of A: 2.5
+	Enter the value of B: .4
+	Enter the value of D: 3
+	The result is: 8.00
+
+*/
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -9,85 +17,101 @@ void push(double);
 double pop();
 double peek();
 
-int main() {
-    char p[100];
-    puts("Enter the postfix expression: ");
-    gets(p);
-    int i = 0;
-    double number, result;
-    puts("\nThe expression is: ");
-    puts(p);
-    strcat(p, " )"); //Add " )" as a sentinel with a forward space
-    while(p[i] != ')') {
-        if(isdigit(p[i]) || p[i] == '.') {
-            int k = 0;
-            char snumber[10] = "";
-            while(p[i] != ' ') { //Form the number as string
-                snumber[k++] = p[i++];
-            }
-            sscanf(snumber, "%lf", &number); //String to integer number
-            push(number);
-        }
-        else {
-            double a, b;
-            if(p[i] == '^') {
-                a = pop();
-                b = pop();
-                push(pow(b, a));
-            }
-            else if(p[i] == '+') {
-                a = pop();
-                b = pop();
-                 push(b+a);
-            }
-            else if(p[i] == '-') {
-                a = pop();
-                b = pop();
-                 push(b-a);
-            }
-            else if(p[i] == '*') {
-                a = pop();
-                b = pop();
-                 push(b*a);
-            }
-            else if(p[i] == '/') {
-                a = pop();
-                b = pop();
-                 push(b/a);
-            }
-            i++;
-        }
-        i++;
-    }
-    printf("\nThe result is %.2lf\n\n", pop());
-    return 0;
+int main()
+{
+	char p[100];
+	puts("Enter the postfix expression: ");
+	gets(p);
+	int i = 0;
+	double number, result;
+	strcat(p, ")"); // Add ")" as a sentinel
+	while (p[i] != ')')
+	{
+		if (isdigit(p[i]) || p[i] == '.')
+		{
+			int k = 0;
+			char snumber[10] = "";
+			while (isdigit(p[i]) || p[i] == '.') // Form the number as string
+			{
+				snumber[k++] = p[i++];
+			}
+			sscanf(snumber, "%lf", &number); // String to double number
+			push(number);
+			continue;
+		}
+		if ((p[i] >= 'A' && p[i] <= 'Z') || (p[i] >= 'a' && p[i] <= 'z'))
+		{
+			printf("\nEnter the value of %c: ", p[i]);
+			double value;
+			scanf("%lf", &value);
+			push(value);
+		}
+		else if (p[i] != ' ')
+		{
+			double a, b;
+			a = pop();
+			b = pop();
+			if (p[i] == '^')
+			{
+				push(pow(b, a));
+			}
+			else if (p[i] == '+')
+			{
+				push(b + a);
+			}
+			else if (p[i] == '-')
+			{
+				push(b - a);
+			}
+			else if (p[i] == '*')
+			{
+				push(b * a);
+			}
+			else if (p[i] == '/')
+			{
+				push(b / a);
+			}
+		}
+		i++;
+	}
+	printf("\nThe result is %.2lf\n\n", pop());
+	return 0;
 }
-void push(double item) {
-    if(top == MAX-1) {
-        puts("\nStack Overflow\n");
-    }
-    else {
-        top++;
-        stack[top] = item;
-    }
+void push(double item)
+{
+	if (top == MAX - 1)
+	{
+		puts("\nStack Overflow\n");
+	}
+	else
+	{
+		top++;
+		stack[top] = item;
+	}
 }
-double pop() {
-    if(top == -1) {
-        puts("\nStack Underflow\n");
-        return -1;
-    }
-    else {
-        double value = stack[top];
-        top--;
-        return value;
-    }
+double pop()
+{
+	if (top == -1)
+	{
+		puts("\nStack Underflow\n");
+		return -1;
+	}
+	else
+	{
+		double value = stack[top];
+		top--;
+		return value;
+	}
 }
-double peek() {
-    if(top == -1) {
-        puts("\nStack is empty\n");
-        return -1;
-    }
-    else {
-        return stack[top];
-    }
+double peek()
+{
+	if (top == -1)
+	{
+		puts("\nStack is empty\n");
+		return -1;
+	}
+	else
+	{
+		return stack[top];
+	}
 }
